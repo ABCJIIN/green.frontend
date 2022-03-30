524ec2466a9155e3df199bb3bb7a742ab5b07837
ABCJIIN <wldls0309@naver.com>
2022-03-30 19:01:05 +0900

    0330

---
+++ html/chapter1/accueweather/js/common.js
@@ -0,0 +1,30 @@
+$(function (){
+    $(".hamburger").click(function(){
+        $(".mobile.hamburger").hide();
+        $(".mobile.close").show();
+
+        $("#mobile_menu").empty();
+
+        var nav = $(".nav").clone();
+        $("#mobile_menu").append(nav);
+        $("#mobile_menu").show();
+    });
+
+    $(".close").click(function(){
+        $(".mobile.hamburger").show();
+        $(".mobile.close").hide();
+
+        $("#mobile_menu").hide();
+    });
+});
+
+$(window).resize(function(){
+    var width = $(window).width();
+    if(width >= 767) {
+        if($("#mobile_menu").is(":visible")) {
+            $(".mobile.hamburger").show();
+            $(".mobile.close").hide();
+            $("#mobile_menu").hide();
+        }
+    }
+});
